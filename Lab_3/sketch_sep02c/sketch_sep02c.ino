712455f6a1b4a1a3dcb30639e22cb7eef7af0c26
Tonkhaowm8 <Tonkhaowm8@gmail.com>
2022-09-07 00:23:18 +0700

    modify arduino code

--- Lab_3/sketch_sep02c/sketch_sep02c.ino
+++ Lab_3/sketch_sep02c/sketch_sep02c.ino
@@ -1,26 +1,30 @@
 #include <EEPROM.h>
- 
-int a = 0;
-int value;
- 
-void setup()
-{
-  Serial.begin(9600);
+
+struct MyObject{
+  char field1[7];
+  char date[11];
+  char company[4];
+};
+
+void setup(){
+
+  float f = 0.00f;   //Variable to store data read from EEPROM.
+  int eeAddress = 0; //EEPROM address to start reading from
+
+  Serial.begin( 9600 );
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+  // get() can be used with custom structures too.
+  eeAddress = sizeof(float); //Move address to the next byte after float 'f'.
+  MyObject customVar; //Variable to store custom object read from EEPROM.
+  EEPROM.get( eeAddress, customVar );
+
+  //Serial.println( customVar.field1 );
+  Serial.println( customVar.date);
+  Serial.println( customVar.company);
 }
+
+void loop(){
   
-void loop()
- {
-  value = EEPROM.read(a);
- 
-  Serial.print(a);
-  Serial.print("\t");
-  Serial.print(value);
-  Serial.println();
-  
-  a = a + 1;
-  
-  if (a == 512)
-    a = 0;
-  
-  delay(500);
 }
--- Lab_3/sketch_sep02d/sketch_sep02d.ino
+++ Lab_3/sketch_sep02d/sketch_sep02d.ino
@@ -1,15 +1,36 @@
 #include <EEPROM.h>
 
+struct MyObject {
+  char field1[7];
+  char date[11];
+  char company[4];
+};
+
 void setup() {
-  // put your setup code here, to run once:
-  String productInfo[] = {"TH01234", "Oct 7 2020", "SIIE"};
-  for(int i = 0; i < 3; i++){
-    EEPROM.put(i, productInfo[i]);
-    delay(1000);
+
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
   }
+
+  int eeAddress = 0;   //Location we want the data to be put.
+
+
+  //One simple call, with the address first and the object second.
+
+
+  /** Put is designed for use with custom structures also. **/
+
+  //Data to store.
+  MyObject customVar = {
+    "TH01234",
+    "Oct 7, 2020",
+    "SIIE"
+  };
+
+  eeAddress += sizeof(float); //Move address to the next byte after float 'f'.
+
+  EEPROM.put(eeAddress, customVar);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
+void loop() {   /* Empty loop */ }
